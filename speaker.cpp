f45d9c301adc99426c14988f62b4ff1df0a09ee7
Secur-I-T <80746881+Secur-I-T@users.noreply.github.com>
2021-03-16 12:01:08 +0100

    Update speaker.cpp

--- speaker.cpp
+++ speaker.cpp
@@ -1,88 +1,24 @@
-/*Arduino speaker song tutorial
-* This program will play the theme song of the Malayalam movie
-* 'Ennu Ninte Moideen'. The song is 'Mukkathe Penne'.
-* The song is played on the speaker connected to pin 3 and GND.
-* 
-* Created 26 Oct 2015
-* by Akshay James
-* Video at https://www.youtube.com/watch?v=LgtcUxe8fmA
-*/
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7);  //Pins où on va connecter l'écran (RS, E, D4, D5, D6, D7)
 
-#include"../include/pitches.h"
-
-// notes in the song 'Mukkathe Penne'
-int melody[] = {
-NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_A4,
-NOTE_G4, NOTE_C5, NOTE_AS4, NOTE_A4,                   
-NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_DS4, NOTE_D4,
-NOTE_C4, NOTE_D4,0,                                 
-
-NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_A4,
-NOTE_G4, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_A4,      //29               //8
-NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_FS4, NOTE_DS4, NOTE_D4,
-NOTE_C4, NOTE_D4,0,                                       
-
-NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_DS5, NOTE_D5,
-NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_C5,
-NOTE_C4, NOTE_D4, NOTE_DS4, NOTE_FS4, NOTE_D5, NOTE_C5,
-NOTE_AS4, NOTE_A4, NOTE_C5, NOTE_AS4,             //58
-
-NOTE_D4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_DS5, NOTE_D5,
-NOTE_C5, NOTE_D5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_C5, NOTE_G4,
-NOTE_A4, 0, NOTE_AS4, NOTE_A4, 0, NOTE_G4,
-NOTE_G4, NOTE_A4, NOTE_G4, NOTE_FS4, 0,
-
-NOTE_C4, NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_DS4,
-NOTE_C4, NOTE_D4, 0,
-NOTE_C4, NOTE_D4, NOTE_G4, NOTE_FS4, NOTE_DS4,
-NOTE_C4, NOTE_D4, END
-
-};
-
-// note durations: 8 = quarter note, 4 = 8th note, etc.
-int noteDurations[] = {       //duration of the notes
-8,4,8,4,
-4,4,4,12,
-4,4,4,4,4,4,
-4,16,4,
-
-8,4,8,4,
-4,2,1,1,2,1,1,12,
-4,4,4,4,4,4,
-4,16,4,
-
-4,4,4,4,4,4,
-4,4,4,12,
-4,4,4,4,4,4,
-4,4,4,12,
-
-4,4,4,4,4,4,
-2,1,1,2,1,1,4,8,4,
-2,6,4,2,6,4,
-2,1,1,16,4,
-
-4,8,4,4,4,
-4,16,4,
-4,8,4,4,4,
-4,20,
-};
-
-int speed=90;  //higher value, slower notes
 void setup() {
-
-Serial.begin(9600);
-for (int thisNote = 0; melody[thisNote]!=-1; thisNote++) {
-
-int noteDuration = speed*noteDurations[thisNote];
-tone(3, melody[thisNote],noteDuration*.95);
-Serial.println(melody[thisNote]);
-
-delay(noteDuration);
-
-noTone(3);
-}
+  lcd.begin(16, 2);   //on initialise l'écran
 }
 
 void loop() {
-// no need to repeat the melody.
+  //Message 1
+  lcd.setCursor(0, 0);          //Place le pointeur sur la colonne 0, ligne 0
+  lcd.write("essai 1:");      //écrit le message 
+  lcd.setCursor(0, 1);         //Place le pointeur sur la colonne 0, ligne 1
+  lcd.write("bonjour!");    //écrit le message
+  delay(3500);                 //attente 3.5 secondes
+  lcd.clear();                 //efface le message affiché
+
+  //Message 2
+  lcd.setCursor(3, 0);          //Place le pointeur sur la colonne 3, ligne 0
+  lcd.write("essai 2:");       //écrit le message
+  lcd.setCursor(2, 1);          //Place le pointeur sur la colonne 2, ligne 1
+  lcd.write("Arduino et LCD");   //écrit le message
+  delay(3500);                  //attente 3.5 secondes
+  lcd.clear();                  //efface le message affiché
 }
