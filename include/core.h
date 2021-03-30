786d42d1ff4459b0b01746be4b1f381e74a0ee72
emmabergman <80757729+emmabergman@users.noreply.github.com>
2021-03-30 08:19:51 +0200

    Update core.h
    updating core.h with predictions function

---
+++ include/core.h
@@ -0,0 +1,14 @@
+#ifndef PREDICTION
+#define PREDICTION
+
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+void predictions(int argc, const char *argv[]);
+
+#endif
