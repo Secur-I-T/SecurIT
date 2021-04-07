7bed844dc3b3ac450cecd1609cc6e9df04d08759
emmabergman <80757729+emmabergman@users.noreply.github.com>
2021-04-07 08:50:42 +0200

    Delete CTestTestfile.cmake

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/SecurIT
-# Build directory: /home/pi/SecurIT/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
